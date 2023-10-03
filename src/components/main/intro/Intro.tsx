982583934825e3798c2b0813d4c525a7225c4b58
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2023-10-03 12:27:26 +0900

    fix. 메인 인트로 페이지 bg를 각 스포츠 이미지가 아닌 전체 이미지를 배경으로 쓰도록 수정

--- src/components/main/intro/Intro.tsx
+++ src/components/main/intro/Intro.tsx
@@ -2,25 +2,21 @@ import { CSC } from 'components/common/styled';
 import { S } from './styled';
 import { useTranslation } from 'react-i18next';
 import { useImageQuery } from 'lib';
-import tennis from 'assets/image/main/tennis.png';
-import golf from 'assets/image/main/golf.png';
-import soccer from 'assets/image/main/soccer.png';
-import martialArts from 'assets/image/main/martial-arts.png';
-import basketball from 'assets/image/main/basketball.png';
+import mainIntroBg from 'assets/background/main/main-bg.png';
 import textKo from 'assets/image/main/text-ko.png';
 import textEn from 'assets/image/main/text-en.png';
 
 const Intro = () => {
   const { i18n } = useTranslation();
   const isKorean = i18n.language === 'kr';
-  const [textImage, tennisImage, golfImage, soccerImage, martialArtsImage, basketballImage] =
-    useImageQuery([
-      'main-intro',
-      [isKorean ? textKo : textEn, tennis, golf, soccer, martialArts, basketball],
-    ]);
+  const [textImage, mainBg] = useImageQuery([
+    'main-intro',
+    [isKorean ? textKo : textEn, mainIntroBg],
+  ]);
 
   return (
     <CSC.PageWrap>
+      <CSC.Background src={mainBg} alt='main-intro-bg' />
       <S.Wrap>
         <S.TextWrap>
           {isKorean ? (
@@ -43,32 +39,6 @@ const Intro = () => {
             />
           )}
         </S.TextWrap>
-        <S.Image top='25rem' left='9rem' width='20rem' height='21rem' imageUrl={tennisImage} />
-        <S.Image top='-11rem' left='19rem' width='24rem' height='24rem' imageUrl={golfImage} />
-        <S.Image
-          top='-30.5rem'
-          left='32rem'
-          width='60rem'
-          height='40.5rem'
-          imageUrl={soccerImage}
-          zIndex={1}
-        />
-        <S.Image
-          top='-80rem'
-          left='73rem'
-          width='23.75rem'
-          height='26rem'
-          imageUrl={martialArtsImage}
-          zIndex={2}
-        />
-        <S.Image
-          top='-92rem'
-          left='87rem'
-          width='24rem'
-          height='24rem'
-          imageUrl={basketballImage}
-          zIndex={1}
-        />
       </S.Wrap>
     </CSC.PageWrap>
   );
