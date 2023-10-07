39f2c6ccf5661cb64b9a7fe5d3278d5837ddc74a
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2023-10-07 16:10:00 +0900

    fix. 메인 인트로 페이지 배경 변경 및 국문 애니메이션 추가

--- src/components/main/intro/IntroText.tsx
+++ src/components/main/intro/IntroText.tsx
@@ -1,4 +1,6 @@
+import { useState, useEffect, useMemo, useRef } from 'react';
 import { S } from './styled';
+import logo from 'assets/image/intro/logo.png';
 
 type PropsType = {
   isKorean: boolean;
@@ -6,18 +8,51 @@ type PropsType = {
 
 const IntroText = (props: PropsType) => {
   const { isKorean } = props;
+  const [textKoOption, setTextKoOption] = useState<boolean>(false);
+  const textKoTimeoutRef = useRef<NodeJS.Timeout>(null);
+  const slideProps = useMemo<{ direction: 'left'; timeout: number }>(
+    () => ({ direction: 'left', timeout: 750 }),
+    []
+  );
+
+  useEffect(() => {
+    //@ts-expect-error interval useRef 설정을 위한 타입 체크 skip
+    textKoTimeoutRef.current = setTimeout(() => {
+      setTextKoOption(!textKoOption);
+    }, 5000);
+  }, [textKoOption]);
+
+  useEffect(() => {
+    return () => {
+      if (!textKoTimeoutRef.current) return;
+      clearTimeout(textKoTimeoutRef.current);
+    };
+  }, []);
 
   return (
     <>
       {isKorean ? (
-        <S.Text top='15rem' width='28.25rem' height='3.25rem'>
-          <span>스포츠, </span>
-          <span>
-            <strong>즐거움</strong>의 <strong>날개</strong>를 펴다
-          </span>
+        <S.Text top='15rem' height='3.25rem'>
+          <S.Slide in={textKoOption} {...slideProps}>
+            <p>
+              <span>스포츠, </span>
+              <span>
+                <strong>즐거움</strong>의 <strong>날개</strong>를 펴다
+              </span>
+            </p>
+          </S.Slide>
+          <S.Slide in={!textKoOption} {...slideProps}>
+            <p>
+              <span>센세이셔널 </span>
+              <S.TextKoWithImage>
+                <strong>스릴부스터</strong>
+                <img src={logo} alt='warp' />
+              </S.TextKoWithImage>
+            </p>
+          </S.Slide>
         </S.Text>
       ) : (
-        <S.Text top='13.75rem' width='40rem' height='2.75rem' isEn={!isKorean}>
+        <S.Text top='13.75rem' height='2.75rem' isEn={!isKorean}>
           <span>IGNITING THE </span>
           <span>
             <strong>THRILL OF SPORTS</strong>
--- src/components/main/intro/styled/styled.ts
+++ src/components/main/intro/styled/styled.ts
@@ -1,4 +1,4 @@
-import { styled } from '@mui/material';
+import { Slide as S, styled } from '@mui/material';
 import { palette } from 'constants/';
 
 export const TextWrap = styled('div')({
@@ -11,10 +11,9 @@ export const TextWrap = styled('div')({
 });
 
 export const Text = styled('div', {
-  shouldForwardProp: (prop: string) => !['top', 'width', 'height', 'isEn'].includes(prop),
-})<ImagePropsType>(({ top, width, height, isEn }) => ({
+  shouldForwardProp: (prop: string) => !['top', 'height', 'isEn'].includes(prop),
+})<ImagePropsType>(({ top, height, isEn }) => ({
   display: 'flex',
-  width,
   height,
   fontSize: `${isEn ? 2 : 2.25}rem`,
   fontWeight: isEn ? 300 : 100,
@@ -25,18 +24,32 @@ export const Text = styled('div', {
   '& span:first-of-type': {
     transform: 'translateY(1rem)',
   },
+  '& p': { display: 'flex', margin: 0 },
 }));
 
-export const ForeText = styled('div')({
-  fontWeight: 300,
-  transform: '',
+export const Slide = styled(S)({
+  position: 'absolute',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '100%',
+  '& span': { width: 'fit-content' },
 });
 
-export const RearText = styled('div')({});
+export const TextKoWithImage = styled('span')({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.5rem',
+  '& img': {
+    width: '9rem',
+    minWidth: '9rem',
+    height: '2.5rem',
+    transform: 'translateY(0.35rem)',
+  },
+});
 
 type ImagePropsType = {
   top: string | number;
-  width: string | number;
   height: string | number;
   isEn?: boolean;
 };
