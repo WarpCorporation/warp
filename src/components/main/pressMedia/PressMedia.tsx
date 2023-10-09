c8c05284aca85adf8ed337f1ae4faf0338b6bf21
J012_김경식 <83746849+kskim625@users.noreply.github.com>
2023-10-09 15:09:21 +0900

    feat. 미디어 페이지 모바일 반응형 구현

--- src/components/main/pressMedia/PressMedia.tsx
+++ src/components/main/pressMedia/PressMedia.tsx
@@ -1,8 +1,13 @@
-import { Header } from 'components/common';
+import { useRecoilValue } from 'recoil';
+import { atom } from 'recoil/atom';
+import { Header, MobileHeader } from 'components/common';
 import { pressData } from 'constants/';
 import { S } from './styled';
 
 const PressMedia = () => {
+  const { type } = useRecoilValue(atom.screen);
+  const title = 'PRESS · MEDIA';
+
   const handleMoreButtonClick = (url: string) => {
     window.open(url, '_blank');
   };
@@ -10,17 +15,18 @@ const PressMedia = () => {
   return (
     <>
       <Header title='PRESS · MEDIA' />
-      <S.Wrap>
-        <S.CardWrap>
+      <S.Wrap type={type}>
+        <S.CardWrap type={type}>
+          <MobileHeader title={title} marginOffset={32} />
           {pressData.map(({ title, url, description, date }) => (
             <S.Card key={description}>
-              <S.CardRow>
+              <S.CardRow type={type}>
                 <S.Title>{title}</S.Title>
                 <S.Date>{date}</S.Date>
               </S.CardRow>
               <S.Divider />
-              <S.Description>{description}</S.Description>
-              <S.ButtonWrap onClick={() => handleMoreButtonClick(url)}>
+              <S.Description type={type}>{description}</S.Description>
+              <S.ButtonWrap type={type} onClick={() => handleMoreButtonClick(url)}>
                 <S.MoreButton>+</S.MoreButton>more
               </S.ButtonWrap>
             </S.Card>
--- src/components/main/pressMedia/styled/styled.ts
+++ src/components/main/pressMedia/styled/styled.ts
@@ -1,23 +1,28 @@
 import { Divider as D, styled } from '@mui/material';
 import { palette, screen } from 'constants/';
 import moreButton from 'assets/image/intro/warp-button.png';
+import { ScreenType } from 'recoil/atom';
 
-export const Wrap = styled('div')({
+export const Wrap = styled('div', {
+  shouldForwardProp: (prop: string) => prop !== 'type',
+})<{ type: ScreenType }>(({ type }) => ({
   display: 'flex',
+  flexGrow: 1,
   justifyContent: 'center',
-  padding: '4.5rem 0',
+  padding: `${type === 'mobile' ? 2.5 : 4.5}rem 0`,
   width: '100vw',
   minWidth: screen.minWidth,
-  height: 'calc(100vh - 25.5rem)',
   overflow: 'hidden auto',
-});
+}));
 
-export const CardWrap = styled('div')({
+export const CardWrap = styled('div', {
+  shouldForwardProp: (prop: string) => prop !== 'type',
+})<{ type: ScreenType }>(({ type }) => ({
   display: 'flex',
   flexDirection: 'column',
-  width: '64rem',
-  minWidth: '64rem',
-});
+  width: type === 'mobile' ? '90vw' : '64rem',
+  minWidth: type === 'mobile' ? '90vw' : '64rem',
+}));
 
 export const Card = styled('div')({
   display: 'flex',
@@ -26,12 +31,17 @@ export const Card = styled('div')({
   width: '100%',
 });
 
-export const CardRow = styled('div')({
+export const CardRow = styled('div', {
+  shouldForwardProp: (prop: string) => prop !== 'type',
+})<{ type: ScreenType }>(({ type }) => ({
   display: 'flex',
-  alignItems: 'flex-end',
+  flexDirection: type === 'mobile' ? 'column' : 'row',
+  alignItems: type === 'mobile' ? 'center' : 'flex-end',
   justifyContent: 'space-between',
   width: '100%',
-});
+  textAlign: type === 'mobile' ? 'center' : 'unset',
+  gap: `${type === 'mobile' ? 0.5 : 0}rem`,
+}));
 
 export const Title = styled('span')({
   fontSize: '1.25rem',
@@ -48,21 +58,26 @@ export const Divider = styled(D)({
   margin: '1rem 0',
 });
 
-export const Description = styled('div')({
+export const Description = styled('div', {
+  shouldForwardProp: (prop: string) => prop !== 'type',
+})<{ type: ScreenType }>(({ type }) => ({
   width: '100%',
   fontSize: '1rem',
   fontWeight: 500,
-});
+  textAlign: type === 'mobile' ? 'center' : 'unset',
+}));
 
-export const ButtonWrap = styled('div')({
+export const ButtonWrap = styled('div', {
+  shouldForwardProp: (prop: string) => prop !== 'type',
+})<{ type: ScreenType }>(({ type }) => ({
   display: 'flex',
   alignItems: 'center',
-  justifyContent: 'flex-end',
+  justifyContent: type === 'mobile' ? 'center' : 'flex-end',
   marginTop: '1rem',
   color: palette.magenta,
   gap: '0.5rem',
   cursor: 'pointer',
-});
+}));
 
 export const MoreButton = styled('div')({
   display: 'flex',
